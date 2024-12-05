e320ff2c35bbdd699d7b186bc03b75fdf4c1cf32
Phillip Madsen <apcoder2019@gmail.com>
2024-12-05 01:58:34 -0700

    updates

---
+++ resources/views/account/layouts/partials/footer.blade.php
@@ -0,0 +1,149 @@
+	<!-- Footer -->
+	<footer class="site-footer style-2">
+		<!-- Footer Top -->
+		<div class="footer-top">
+			<div class="container">
+				<div class="row">
+					<div class="col-xl-3 col-md-4 col-sm-6 wow fadeInUp" data-wow-delay="0.1s">
+						<div class="widget widget_about me-2">
+							<div class="footer-logo logo-white">
+								<a href="index.html"><img src="images/logo.svg" alt=""></a> 
+							</div>
+							<ul class="widget-address">
+								<li>
+									<p><span>Address</span> : 451 Wall Street, UK, London</p>
+								</li>
+								<li>
+									<p><span>E-mail</span> : <a href="mailto:example@info.com">example@info.com</a></p>
+								</li>
+								<li>
+									<p><span>Phone</span> : <a href="tel:+9912121211">+919912121211</a></p>
+								</li>
+							</ul>
+							<h2 class="footer-title">Social Profile</h2>
+							<div class="dz-social-icon">
+								<ul>
+									<li>
+										<a href="https://www.facebook.com/dexignzone" target="_blank">
+											<i class="fa-brands fa-facebook-f"></i>
+										</a>
+									</li>
+									<li>
+										<a href="https://twitter.com/dexignzones" target="_blank">
+											<i class="fa-brands fa-twitter"></i>
+										</a>
+									</li>
+									<li>
+										<a href="https://www.linkedin.com/showcase/3686700/admin/" target="_blank">
+											<i class="fa-brands fa-linkedin"></i>
+										</a>
+									</li>
+									<li>
+										<a href="https://www.instagram.com/dexignzone/" target="_blank">
+											<i class="fa-brands fa-instagram"></i>
+										</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="col-xl-3 col-md-4 col-sm-6 wow fadeInUp" data-wow-delay="0.2s">
+						<div class="widget widget_post">
+							<h2 class="footer-title">recent Social media post</h2>
+							<ul>
+								<li>
+									<div class="dz-media">
+										<a href="post-standard.html"><img src="images/shop/product/small/1.png" alt=""></a>
+									</div>
+								</li>
+								<li>
+									<div class="dz-media">
+										<a href="post-standard.html"><img src="images/shop/product/small/2.png" alt=""></a>
+									</div>
+								</li>
+								<li>
+									<div class="dz-media">
+										<a href="post-standard.html"><img src="images/shop/product/small/3.png" alt=""></a>
+									</div>
+								</li>
+								<li>
+									<div class="dz-media">
+										<a href="post-standard.html"><img src="images/shop/product/small/4.png" alt=""></a>
+									</div>
+								</li>
+								<li>
+									<div class="dz-media">
+										<a href="post-standard.html"><img src="images/shop/product/small/5.png" alt=""></a>
+									</div>
+								</li>
+								<li>
+									<div class="dz-media">
+										<a href="post-standard.html"><img src="images/shop/product/small/6.png" alt=""></a>
+									</div>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-xl-2 col-md-4 col-sm-4 col-6 wow fadeInUp" data-wow-delay="0.3s">
+						<div class="widget widget_services">
+							<h2 class="footer-title">Features</h2>
+							<ul>
+								<li><a href="javascript:void(0);">E-Money</a></li>
+								<li><a href="javascript:void(0);">Local Business</a></li>
+								<li><a href="javascript:void(0);">Corporate</a></li>
+								<li><a href="javascript:void(0);">Internet Money</a></li>
+								<li><a href="javascript:void(0);">Latest News</a></li>
+								<li><a href="javascript:void(0);">Staking Ecosystem</a></li>
+							</ul>   
+						</div>
+					</div>
+					<div class="col-xl-2 col-md-4 col-sm-4 col-6 wow fadeInUp" data-wow-delay="0.4s">
+						<div class="widget widget_services">
+							<h2 class="footer-title">Useful Links</h2>
+							<ul>
+								<li><a href="javascript:void(0);">Privacy Policy</a></li>
+								<li><a href="javascript:void(0);">Returns</a></li>
+								<li><a href="javascript:void(0);">Terms & Conditions</a></li>
+								<li><a href="javascript:void(0);">Contact Us</a></li>
+								<li><a href="javascript:void(0);">Latest News</a></li>
+								<li><a href="javascript:void(0);">Our Sitemap</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-xl-2 col-md-4 col-sm-4 wow fadeInUp" data-wow-delay="0.5s">
+						<div class="widget widget_services">
+							<h2 class="footer-title">Footer Menu</h2>
+							<ul>
+								<li><a href="javascript:void(0);">Instagram profile</a></li>
+								<li><a href="javascript:void(0);">New Collection</a></li>
+								<li><a href="javascript:void(0);">Popular Plant </a></li>
+								<li><a href="javascript:void(0);">Contact Us</a></li>
+								<li><a href="javascript:void(0);">Latest News</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<img class="Plant-img wow fadeInLeft" data-wow-delay="0.8s" src="images/Plant-1.png" alt="">
+			<img class="Plant-img2 wow fadeInRight" data-wow-delay="0.8s" src="images/Plant-2.png" alt="">
+		</div>
+		<!-- Footer Top End -->
+		
+		<!-- Footer Bottom -->
+		<div class="footer-bottom">
+			<div class="container">
+				<div class="row fb-inner wow fadeInUp" data-wow-delay="0.1s">
+					<div class="col-lg-6 col-md-12 text-start"> 
+						<p class="copyright-text">© <span class="current-year">2024</span> <a href="https://www.dexignzone.com/">DexignZone</a> Theme. All Rights Reserved.</p>
+					</div>
+					<div class="col-lg-6 col-md-12 text-end"> 
+						<div class="d-flex align-items-center justify-content-center justify-content-md-center justify-content-xl-end">
+							<img src="images/footer-img2.png" alt="">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- Footer Bottom End -->
+	</footer>
+	<!-- Footer End -->
