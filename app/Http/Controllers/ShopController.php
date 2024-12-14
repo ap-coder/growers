f81207a3e95d47cb782a1bc0d1c4f57dbfa74006
Phillip Madsen <apcoder2019@gmail.com>
2024-12-13 23:59:57 -0700

    Update ShopController.php

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -19,7 +19,12 @@ class ShopController extends Controller
      */
     public function index()
     {
-        $products = Product::with('media')->paginate(12);
+        $client = auth()->user(); // or however you get the current client
+
+        // Only get products that have a ClientPrice for this specific client
+        $products = Product::whereHas('clientPrices', function($query) use ($client) {
+            $query->where('client_id', $client->id);
+        })->with('media')->paginate(12);
 
         return view('shop.index', compact('products'));
     }
