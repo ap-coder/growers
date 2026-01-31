ae65d2ea7fc56284d9dee642c88fb32f65873139
Phillip Madsen <pmadsen@proofpoint.com>
2026-01-31 15:13:31 -0700

    fixed seeders

--- database/seeders/PermissionRoleTableSeeder.php
+++ database/seeders/PermissionRoleTableSeeder.php
@@ -11,23 +11,32 @@ class PermissionRoleTableSeeder extends Seeder
     public function run()
     {
         $all_permissions = Permission::all();
-        
+
         // Admin (id: 1) - All permissions
-        Role::findOrFail(1)->permissions()->sync($all_permissions->pluck('id'));
-        
+        $admin = Role::find(1);
+        if ($admin) {
+            $admin->permissions()->sync($all_permissions->pluck('id'));
+        }
+
         // WCL-Developer (id: 3) - All permissions (same as Admin)
         $wclDeveloper = Role::find(3);
         if ($wclDeveloper) {
             $wclDeveloper->permissions()->sync($all_permissions->pluck('id'));
         }
-        
+
         // Customer (id: 2) - Frontend/account only, NO admin access
-        // Only gets: profile_password_edit (for account management)
+        // Only gets: minimal frontend/account permissions
         $customer_permissions = $all_permissions->filter(function ($permission) {
             return in_array($permission->title, [
                 'profile_password_edit',
+                // Keep in sync with RolesTableSeeder minimal site permissions
+                'site_product_access',
+                'site_product_show',
             ]);
         });
-        Role::findOrFail(2)->permissions()->sync($customer_permissions->pluck('id'));
+        $customer = Role::find(2);
+        if ($customer) {
+            $customer->permissions()->sync($customer_permissions->pluck('id'));
+        }
     }
 }
--- database/seeders/RolesTableSeeder.php
+++ database/seeders/RolesTableSeeder.php
@@ -11,23 +11,37 @@ class RolesTableSeeder extends Seeder
 
     public function run()
     {
+        // Create roles first
+        $admin = Role::firstOrCreate(
+            ['id' => 1],
+            ['title' => 'Admin']
+        );
+
+        $customer = Role::firstOrCreate(
+            ['id' => 2],
+            ['title' => 'Customer']
+        );
+
+        $wclDeveloper = Role::firstOrCreate(
+            ['id' => 3],
+            ['title' => 'WCL Developer']
+        );
+
+        // Now assign permissions
         $all_permissions = Permission::all();
 
-        Role::findOrFail(1)->permissions()->sync($all_permissions->pluck('id'));
+        // Grant all permissions to Admin
+        $admin->permissions()->sync($all_permissions->pluck('id'));
 
-        $wclDeveloper = Role::find(3);
-        if ($wclDeveloper) {
-            $wclDeveloper->permissions()->sync($all_permissions->pluck('id'));
-        }
+        // Grant all permissions to WCL Developer
+        $wclDeveloper->permissions()->sync($all_permissions->pluck('id'));
 
+        // Grant selected permissions to Customer
         $customer_permissions = $all_permissions->filter(function ($permission) {
             return in_array($permission->title, [
                 'profile_password_edit',
-                // Frontend product viewing permissions
-                'site_product_access',
-                'site_product_show',
             ]);
         });
-        Role::findOrFail(2)->permissions()->sync($customer_permissions->pluck('id'));
+        $customer->permissions()->sync($customer_permissions->pluck('id'));
     }
 }
